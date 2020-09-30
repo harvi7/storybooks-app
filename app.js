cab9becb1cb91bf6355d969696219e7bf17a9f67
Harsh Virani <iamharshvirani7@gmail.com>
2020-09-29 17:02:33 -0700

    Auth Middleware & store Sessions in DB

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
 
 const connectDb = require('./config/db') 
 
@@ -31,8 +34,9 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
-    
+
 // Passport middleware 
 app.use(passport.initialize())
 app.use(passport.session())
