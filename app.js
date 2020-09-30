aa9d5007d9f8550a99f5a79808519969f8539c01
Harsh Virani <iamharshvirani7@gmail.com>
2020-09-30 13:30:25 -0700

    Add Story & Dashboard and Public Stories

--- app.js
+++ app.js
@@ -21,12 +21,26 @@ connectDb()
 
 const app = express()
 
+// Body Parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// Handlebars Helpers
+const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.engine('.hbs', exphbs({ helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+    }, 
+    defaultLayout: 'main', extname: '.hbs'}
+))
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -47,6 +61,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
